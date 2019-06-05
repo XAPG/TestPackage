95923375eea8bd0f37ec75e41053aa2f5af6db31
Biglazy <qihongchuan@vchangyi.com>
2019-06-05 13:16:23 +0800

    Third commit.

--- src/SDK/Weather.php
+++ src/SDK/Weather.php
@@ -14,31 +14,34 @@ class Weather
 
     private $config = [];
 
-    public function __construct($config)
+    public function __construct($config = [])
     {
         // 可以从框架的 config/*.php 或者 package 的 config.php 文件中加载配置，以package的config.php为例
         $defaultConfig = require __DIR__. "/../config.php";
         $this->config = array_merge($defaultConfig, $config);
     }
 
-
-    public function getWeather()
+    public function getWeather($address)
     {
-        // 初始化 config 配置等等
-
         $client = new Client([
             'base_uri' => 'http://api.map.baidu.com/',
             'timeout'  => 5.0,
         ]);
 
-        $path = 'geocoder/v2/?address=西安市雁塔区财富中心&output=json&ak='.$this->config['app_secret'];
-        $response = $client->request('GET', $path);
+        $path = 'geocoder/v2/';
+        $options = ['query' =>
+            [
+                'address' => $address ?? '西安市雁塔区财富中心',
+                'output' => 'json',
+                'ak' => $this->config['app_secret']
+            ]
+        ];
+        $response = $client->request('GET', $path, $options);
 
         $result = $response->getBody()->getContents();
-        $result = \GuzzleHttp\json_decode($result);
+        $result = \GuzzleHttp\json_decode($result, true);
 
         return $result;
     }
-
 
 }
